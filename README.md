4a3fb4b54bcf79dc1ebdde1f2929b54dae061501
caribbeanyute <smithcleon@gmail.com>
2020-02-26 17:33:24 +0000

    Began setup of DB

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Lab 5 Starter Code
 Starter code for Lab 5
+#user: lab5
+#pass: info3180lab5
--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
+import secrets
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "change this to be a more random key"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://user:password@localhost/database"
+#app.config['SECRET_KEY'] = "change this to be a more random key"
+app.config['SECRET_KEY'] = secrets.token_urlsafe(32)
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://lab5:info3180lab5@localhost/lab5"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 
 db = SQLAlchemy(app)
